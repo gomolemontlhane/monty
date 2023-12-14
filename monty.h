e76fc3995f0d0bd7d825a41dbeb64017272766a8
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-14 06:35:59 -0800

    fixing compilation errors

--- monty.h
+++ monty.h
@@ -2,15 +2,14 @@
 #define MONTY_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- *struct stack_s - Doubly linked list representation of a stack (or queue)
- *@n: Integer
- *@prev: Points to the previous element of the stack (or queue)
- *@next: Points to the next element of the stack (or queue)
+ *struct stack_s - doubly linked list representation of a stack (or queue)
+ *@n: integer
+ *@prev: points to the previous element of the stack (or queue)
+ *@next: points to the next element of the stack (or queue)
  *
- *Description: Doubly linked list node structure
+ *Description: doubly linked list node structure
  *for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
@@ -21,11 +20,27 @@ typedef struct stack_s
 } stack_t;
 
 /**
- *Function prototypes
+ *struct instruction_s - opcode and its function
+ *@opcode: the opcode
+ *@f: function to handle the opcode
+ *
+ *Description: opcode and its function
+ *for stack, queues, LIFO, FIFO
  */
+typedef struct instruction_s
+{
+	char *opcode;
+	void(*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/*Function prototypes */
 void push(stack_t **stack, unsigned int line_number, int n);
 void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
 void parse_and_execute(stack_t **stack, char *line, unsigned int line_number);
-
 
 #endif /*MONTY_H */
