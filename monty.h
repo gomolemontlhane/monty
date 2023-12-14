6c89503d76ffc6e7bb00c967f2b312992187ff5a
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-14 08:00:11 -0800

    fixing getty errors

--- monty.h
+++ monty.h
@@ -30,7 +30,7 @@ typedef struct stack_s
 typedef struct instruction_s
 {
 	char *opcode;
-	void(*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /*Function prototypes */
@@ -43,5 +43,6 @@ void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void parse_and_execute(stack_t **stack, char *line, unsigned int line_number);
 void free_stack(stack_t *stack);
+
 
 #endif /*MONTY_H */
