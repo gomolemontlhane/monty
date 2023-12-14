eeeec6a3ac0c80455a97b48c0c42457ee602c3aa
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-13 17:49:30 -0800

    added Monty bytecode file 00.m

--- monty.h
+++ monty.h
@@ -1,41 +1,27 @@
 #ifndef MONTY_H
 #define MONTY_H
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *struct stack_s - doubly linked list representation of a stack (or queue)
- *@n: integer
- *@prev: points to the previous element of the stack (or queue)
- *@next: points to the next element of the stack (or queue)
+ *struct stack_s - Doubly linked list representation of a stack (or queue)
+ *@n: Integer
+ *@prev: Points to the previous element of the stack (or queue)
+ *@next: Points to the next element of the stack (or queue)
  *
- *Description: doubly linked list node structure
+ *Description: Doubly linked list node structure
  *for stack, queues, LIFO, FIFO
  */
 typedef struct stack_s
 {
 	int n;
-	struct stack_s * prev;
-	struct stack_s * next;
-}
-
-stack_t;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
 
 /**
- *struct instruction_s - opcode and its function
- *@opcode: the opcode
- *@f: function to handle the opcode
- *
- *Description: opcode and its function
- *for stack, queues, LIFO, FIFO
+ *Function prototypes
  */
-typedef struct instruction_s
-{
-	char *opcode;
-	void(*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-/*Function prototypes */
 void push(stack_t **stack, unsigned int line_number, int n);
 void pall(stack_t **stack, unsigned int line_number);
 
