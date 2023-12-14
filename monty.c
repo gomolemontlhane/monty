97a22ed8d2309d996794bf4311d2f79c5e671288
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-14 15:38:29 -0800

    new  branch

--- monty.c
+++ monty.c
@@ -89,7 +89,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (! *stack)
+	if (!*stack)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		exit(EXIT_FAILURE);
