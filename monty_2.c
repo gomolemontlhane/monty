5e0261f9e25583872dd1d8d183707c62f4a9dd65
gomolemontlhane <gomolemontlhane@gmail.com>
2023-12-14 16:43:05 -0800

    adding files

--- monty_2.c
+++ monty_2.c
@@ -29,7 +29,7 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (! *stack || !(*stack)->next)
+	if (!*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
